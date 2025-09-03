e8f55e7c78503695cfb031b8d35033c43d1af4a0
Jeppe Steen Nielsen <jsndesire@live.dk>
2025-09-03 20:03:12 +0200

    Kunne ikke finde ud af factorial, så blev nødtil at skippe den

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let x = 0;
+  for (let i = 0; i < array.length; i++)
+    x += array[i]
+  return x;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+let x = 1;
+for (let i = 0; i < array.length; i++)
+  x *= array[i]
+return x;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(f) {
+	let res = 1;
+  for (let i = f; i > 0; i--)
+    res = 1 * 4 =
 };
 
 // Do not edit below this line
