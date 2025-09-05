d26222454da81e80bf03f0d02bdfa0d2781a4d56
Jeppe Steen Nielsen <jsndesire@live.dk>
2025-09-05 20:19:20 +0200

    fibonacci, kalrede næsten det hele

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+    if (num < 0) {
+        return "OOPS"
+    }
+        else {
+    for (let i = 2; i <= num; i++) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+    }
+    return num2;
+}
 
 };
 
