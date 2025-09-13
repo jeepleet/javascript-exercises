05c0d7720f3409b6c13050b021231c4317a755e4
Jeppe Steen Nielsen <jsndesire@live.dk>
2025-09-13 18:49:06 +0200

    Step 2 færdig

---
+++ rock-paper-scissors/index.js
@@ -0,0 +1,19 @@
+function getCmomputerChoice() {
+      let str1 = Math.floor((Math.random() * 3) +0);
+   if (str1 === 0) {
+    return "rock";
+    } else if (str1 === 1) {
+    return "paper";
+     } else if (str1 === 2) {
+    return "scissors";
+   
+   } 
+
+
+   }
+console.log(getCmomputerChoice());
+
+
+   
+
+
