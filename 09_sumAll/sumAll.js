9c3267c8b1b9e0bd19e7f55ed22a86803a5c59e7
Jeppe Steen Nielsen <jsndesire@live.dk>
2025-09-23 19:35:11 +0200

    Ny version

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,16 +1,21 @@
 const sumAll = function(num1, num2) {
-    if (typeof num1 !== 'number' || typeof num2 !== 'number' || num1 < 0 || num2 < 0 || !Number.isInteger(num1 || num2))
-    return "ERROR";
+    let start = Math.min(num1, num2);
+    let end = Math.max(num1, num2);
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 <0) {
+        return "ERROR";
+    } else {
+        let sum = 0;
+        let i = start;
+        while (i <= end) {
+            sum += i;
+            i++;
 
-
-    let sum = 0;
-    for (let i = num1; i <= num2; i++) {
-    sum += i;
+        }
+        return sum;
     }
-    return sum;
-   
 
-};
+    };
+   
 
 // Do not edit below this line
 module.exports = sumAll;
