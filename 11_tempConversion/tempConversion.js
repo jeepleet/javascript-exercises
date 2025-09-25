72117cceddbcdc25be50f0a534f6bbd63633f9d3
Jeppe Steen Nielsen <jsndesire@live.dk>
2025-09-25 18:36:56 +0200

    Opdaterede den lige

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,14 +1,14 @@
 const convertToCelsius = function(f) {
-  let celsius = (f - 32) * 5/9
-  celsius = Math.round(celsius * 10) / 10
-  return celsius;
-};
+  let celcius = (f - 32) * 5/9
+  celcius = Math.round(celcius * 10) / 10
+  return celcius;
+}
 
 const convertToFahrenheit = function(c) {
   let fahrenheit = (c * 9/5) + 32
   fahrenheit = Math.round(fahrenheit * 10) / 10
   return fahrenheit;
-};
+}
 
 // Do not edit below this line
 module.exports = {
